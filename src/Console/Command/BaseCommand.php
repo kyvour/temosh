792fadd6725c1d06dfd983e83b41a24a08149882
Andrew Yustyk <a.yustyk@gmail.com>
2017-09-10 17:26:19 +0300

    Update console command

--- src/Console/Command/BaseCommand.php
+++ src/Console/Command/BaseCommand.php
@@ -3,13 +3,11 @@
 namespace Temosh\Console\Command;
 
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Exception\LogicException;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputDefinition;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Temosh\Console\MongoShellInterface;
 use Temosh\Mongo\Client\Client;
 use Temosh\Mongo\Connection\Options;
 
@@ -25,18 +23,6 @@ abstract class BaseCommand extends Command
      * @var \Temosh\Mongo\Client\Client
      */
     private $mongoClient;
-
-    /**
-     * {@inheritdoc}
-     */
-    public function __construct($name = null) {
-        parent::__construct($name);
-
-        // Check for the appropriate Application instance.
-        if (!($this->getApplication() instanceof MongoShellInterface)) {
-            throw new LogicException('Application instance should implement MongoShellInterface');
-        }
-    }
 
     /**
      * @param \Symfony\Component\Console\Input\InputInterface $input
@@ -60,6 +46,8 @@ abstract class BaseCommand extends Command
      */
     protected function configure()
     {
+        parent::configure();
+
         $definition = new InputDefinition([
             $this->hostOption(),
             $this->portOption(),
--- src/Console/Command/ReadCommand.php
+++ src/Console/Command/ReadCommand.php
@@ -3,8 +3,10 @@
 namespace Temosh\Console\Command;
 
 use MongoDB\Driver\Exception\Exception;
+use Symfony\Component\Console\Exception\LogicException;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Temosh\Console\MongoShellInterface;
 
 /**
  * Class SelectCommand
@@ -19,6 +21,14 @@ class ReadCommand extends BaseCommand
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        /** @var \Temosh\Console\MongoShellInterface $app */
+        $app = $this->getApplication();
+        if (!($app instanceof MongoShellInterface)) {
+            throw new LogicException('Application instance should implement MongoShellInterface');
+        }
+
+        /** @var \Temosh\Console\Helper\QuestionHelperInterface $helper */
+        $helper = $this->getHelper('temosh_question');
 
         // Check connection to the MongoDB database.
         try {
@@ -33,29 +43,30 @@ class ReadCommand extends BaseCommand
             return;
         }
 
-        /** @var \Temosh\Console\Helper\QuestionHelperInterface $helper */
-        $helper = $this->getHelper('temosh_question');
-
         // Infinite loop of user queries.
         do {
+            // Get the sql query from user input.
+            $queryString = $helper->askQuery($input, $output);
+            $query = $app->getSqlQuery()->setQueryString($queryString);
+
+            // Do nothing if input is empty.
+            if ($query->isEmpty()) {
+                continue;
+            }
+
+            // Exit on "exit" query.
+            if ($query->isExitCommand()) {
+                return;
+            }
+
             try {
-                // Get the sql query.
-                $query = $helper->askQuery($input, $output);
-
-                // Exit on "exit" query.
-                // @todo move this condition to separate class.
-                if (rtrim(trim($query), '();') === 'exit') {
-                    return;
-                }
-
+                $sqlQueryArray = $query->parse();
                 // Execute query.
-                $output->writeln('Dummy output...');
+                $output->writeln(var_dump($sqlQueryArray));
             } catch (\Exception $e) {
                 // Notify user about exception (invalid query, bad syntax etc.)
                 $output->writeln('<error>' . $e->getMessage() . '</error>');
             }
-
         } while (true);
-
     }
 }
